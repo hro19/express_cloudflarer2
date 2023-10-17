e1503eda94d296f80255a45376cd99440c5ea0f8
hori <fourbasic19@gmail.com>
2023-10-17 16:52:51 +0900

    uploadのPOSTメソッドAPIを作成

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import multer from 'multer';
+import cors from 'cors';
+
+const app = express();
+const port = 5000;
+app.use(cors());
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log(req.file);
+  res.send('File Upload');
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
