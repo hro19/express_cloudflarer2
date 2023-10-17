beeeaf65a166a3694e6190209a96610a28d957d6
hori <fourbasic19@gmail.com>
2023-10-17 20:38:15 +0900

    送信後の通信ステータスを返却

--- index.js
+++ index.js
@@ -29,15 +29,21 @@ app.post('/api/upload', upload.single('file'), async (req, res) => {
   .resize({ width: 700 })
   .toBuffer();
 
-  await S3.send(
-    new PutObjectCommand({
-      Body: resizeFileBuffer,
-      Bucket: 'reffect',
-      Key: req.file.originalname,
-      ContentType: req.file.mimetype,
-    })
-  );
-  res.send('File Upload');
+  try {
+    await S3.send(
+      new PutObjectCommand({
+        Body: req.file.buffer,
+        Bucket: 'reffect',
+        Key: req.file.originalname,
+        ContentType: req.file.mimetype,
+      })
+    );
+    // ファイルアップロードが成功した場合、JSONレスポンスを返す
+    res.status(200).json({ httpStatus: 200, message: 'File Upload Successful' });
+  } catch (error) {
+    // エラーが発生した場合、JSONレスポンスを返す
+    res.status(500).json({ httpStatus: 500, message: 'File Upload Failed' });
+  }
 });
 
 app.listen(port, function () {
